d70a5179aba46246079fa68527acc7a740e0bab9
Jonathan Müller <jo-m@outlook.com>
2022-06-07 00:24:29 +0200

    linter

--- pkg/printer/printer_test.go
+++ pkg/printer/printer_test.go
@@ -8,6 +8,6 @@ import (
 
 func TestChunkifyBytes(t *testing.T) {
 	assert.Equal(t,
-		[][]byte{[]byte{1, 2, 3}, []byte{4, 5, 6}, []byte{7, 8, 9}, []byte{10, 11}},
+		[][]byte{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11}},
 		chunkifyBytes([]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, 3))
 }
